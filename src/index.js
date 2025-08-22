f2f17de93080466e31f2eef8d125fac5fa4e907b
Guddu Kumar Yadav <001coderboy@gmail.com>
2025-08-23 00:24:39 +0530

    Completed basic sequelize setup for create a db

--- src/index.js
+++ src/index.js
@@ -1,10 +1,18 @@
 const express = require('express');
 const {PORT} = require('./config/serverConfig');
+const bodyParser = require('body-parser');
 
 const setupAndStartServer = async () =>{
     
     //create the express object
     const app = express();
+
+    // tells Express to parse incoming requests with JSON payloads
+    app.use(bodyParser.json()); // middleware
+
+    //tells Express to parse URL-encoded payloads (like from HTML forms).
+    app.use(bodyParser.urlencoded({extended:true})); // middleware
+    
     app.listen(PORT,()=>{
         console.log(`Server started at ${PORT}`);
         console.log(PORT);
